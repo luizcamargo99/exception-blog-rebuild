17d3df4f05267c8f3ac601c130affe089968c906
Luiz Camargo <luiz.zito@hotmail.com>
2022-09-08 13:56:44 +0100

    Adicionar arquivos de projeto.

---
+++ ExceptionBlogRebuild/Program.cs
@@ -0,0 +1,23 @@
+﻿
+ExceptionDivideByZeroWithTryCatch();
+
+
+void ExceptionDivideByZeroWithTryCatch()
+{
+    try
+    {
+        PrintDivisionByZero();
+    }
+    catch
+    {
+        Console.WriteLine("Ocorreu um erro");
+    }
+
+}
+
+void PrintDivisionByZero()
+{
+    var numberOne = 10;
+    var numberTwo = 0;
+    Console.WriteLine(numberOne / numberTwo);
+}
