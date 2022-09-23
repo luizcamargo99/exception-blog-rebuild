a13078504cb5895001ff858115a0523b2af1edad
Luiz Camargo <luiz.zito@hotmail.com>
2022-09-23 17:28:55 +0100

    finally

---
+++ FinallyException/Program.cs
@@ -0,0 +1,18 @@
+﻿
+CalculateNumbers(2, 0);
+
+void CalculateNumbers(int numberOne, int numberTwo)
+{
+    try
+    {
+        Console.WriteLine(numberOne / numberTwo);
+    }
+    catch
+    {
+        Console.WriteLine("Entrou em catch");
+    }
+    finally
+    {
+        Console.WriteLine("Entrou em finally");
+    }
+}
